842552d24d7177a5c9dd2432f5a737c55a32c5ba
ujihisa <ujihisa@gmail.com>
2009-05-17 13:51:42 -0700

    Added Net::HTTP.put spec

--- blogger_spec.rb
+++ blogger_spec.rb
@@ -1,16 +1,29 @@
 require 'blogger.rb'
 
-describe 'Net::HTTP.post' do
-  it 'wraps posting using .new, #post and #body' do
-    nh = mock('nh')
-    Net::HTTP.should_receive(:new).with('aaa.bbb', 80).and_return(nh)
-    def nh.use_ssl=(a); end
-    def nh.verify_mode=(a); end
-    def nh.post(a, b, c); 'ok' end
-    Net::HTTP.post(
-      'http://aaa.bbb/ccc/ddd',
-      'data',
-      {'a' => 'b'}).should == 'ok'
+describe 'Net::HTTP' do
+  describe '.__post_or_put__' do
+    it 'wraps posting using .new, #post and #body' do
+      nh = mock('nh')
+      Net::HTTP.should_receive(:new).with('aaa.bbb', 80).and_return(nh)
+      def nh.use_ssl=(a); end
+      def nh.verify_mode=(a); end
+      def nh.post(a, b, c); 'ok' end
+      Net::HTTP.__post_or_put__(
+        :post,
+        'http://aaa.bbb/ccc/ddd',
+        'data',
+        {'a' => 'b'}).should == 'ok'
+    end
+  end
+
+  describe '.post, .put' do
+    it 'wrap __post_or_put__' do
+      Net::HTTP.should_receive(:__post_or_put__).with(:post, nil, nil, nil)
+      Net::HTTP.post(nil, nil, nil)
+
+      Net::HTTP.should_receive(:__post_or_put__).with(:put, nil, nil, nil)
+      Net::HTTP.put(nil, nil, nil)
+    end
   end
 end
 

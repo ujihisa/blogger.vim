80a7b3f495a187e1f6f73c0e9eea8c7a206836ae
ujihisa <ujihisa@gmail.com>
2009-08-15 21:40:50 -0700

    Clipped out net/https wrapper library and use it

--- blogger.rb
+++ blogger.rb
@@ -4,26 +4,7 @@ require 'uri'
 require 'rubygems'
 require 'nokogiri'
 require 'markdown'
-
-class Net::HTTP
-  def self.post(uri, data, header)
-    __post_or_put__(:post, uri, data, header)
-  end
-
-  def self.put(uri, data, header)
-    __post_or_put__(:put, uri, data, header)
-  end
-
-  def self.__post_or_put__(method, uri, data, header)
-    uri = URI.parse(uri)
-    i = new(uri.host, uri.port)
-    unless uri.port == 80
-      i.use_ssl = true
-      i.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    end
-    i.__send__(method, uri.path, data, header)
-  end
-end
+require 'net-https-wrapper'
 
 class Array
   # maph :: [a] -> (a -> b) -> Hash
--- blogger_spec.rb
+++ blogger_spec.rb
@@ -6,33 +6,6 @@ class DateTime
   end
 end
 
-describe 'Net::HTTP' do
-  describe '.__post_or_put__' do
-    it 'wraps posting using .new, #post and #body' do
-      nh = mock('nh')
-      Net::HTTP.should_receive(:new).with('aaa.bbb', 80).and_return(nh)
-      def nh.use_ssl=(a); end
-      def nh.verify_mode=(a); end
-      def nh.post(a, b, c); 'ok' end
-      Net::HTTP.__post_or_put__(
-        :post,
-        'http://aaa.bbb/ccc/ddd',
-        'data',
-        {'a' => 'b'}).should == 'ok'
-    end
-  end
-
-  describe '.post, .put' do
-    it 'wrap __post_or_put__' do
-      Net::HTTP.should_receive(:__post_or_put__).with(:post, nil, nil, nil)
-      Net::HTTP.post(nil, nil, nil)
-
-      Net::HTTP.should_receive(:__post_or_put__).with(:put, nil, nil, nil)
-      Net::HTTP.put(nil, nil, nil)
-    end
-  end
-end
-
 describe 'Array#maph' do
   it 'wraps Enumerable#map to get Hash directly' do
     hash = [1, 2, 3].maph {|i| [i.to_s, i*2] }
